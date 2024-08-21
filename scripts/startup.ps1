ef3a75ac9138c79301f6a5ead4e7ee1adf4a366a
AJ Stangl <alfred.stangl@crisphealth.org>
2024-08-21 17:17:19 -0400

    Updating things to be agnostic

--- scripts/startup.ps1
+++ scripts/startup.ps1
@@ -1,9 +1,8 @@
 # Define the URL of the JDK package
 $jdkUrl = "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip"
 
-
 # Set the target installation path
-$installPath = "C:\Users\Alfred.Stangl\repos\personal\FHIR.Starter\jdk"
+$installPath = "jdk"
 
 # Create the installation directory if it does not exist
 if (-Not (Test-Path -Path $installPath)) {
@@ -11,19 +10,10 @@ if (-Not (Test-Path -Path $installPath)) {
 }
 
 # Define the path for the downloaded file
-$downloadedFile = "C:\Users\Alfred.Stangl\repos\personal\FHIR.Starter\jdk.zip"
+$downloadedFile = "jdk.zip"
 
 # Download the JDK zip file
 Invoke-WebRequest -Uri $jdkUrl -OutFile $downloadedFile
 
 # Extract the JDK zip file
 Expand-Archive -Path $downloadedFile -DestinationPath $installPath -Force
-
-# # Set JAVA_HOME environment variable
-# $env:JAVA_HOME = "$installPath\jdk-11.0.2"
-#
-# # Add Java to the PATH environment variable
-# $env:Path = "$env:JAVA_HOME\bin;$env:Path"
-
-# Verify Java installation
-# java -version
